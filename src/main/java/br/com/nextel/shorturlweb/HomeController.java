7b2b9fc2fe720765246fba67d2beb3f7d06b345a
Wagner Ferreira Alves <wagnerferreirasp@hotmail.com>
2018-07-22 22:39:47 -0300

    Ajustes

--- src/main/java/br/com/nextel/shorturlweb/HomeController.java
+++ src/main/java/br/com/nextel/shorturlweb/HomeController.java
@@ -1,15 +1,27 @@
 package br.com.nextel.shorturlweb;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        int a = 0;
-        int b= 1;
         return "home";
+    }
+
+    @PostMapping("/encurta")
+    @ResponseBody
+    public ResponseEntity<String> encurta(@RequestBody String url) {
+        RestTemplate restTemplate = new RestTemplateBuilder().build();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://localhost:8091", url, String.class);
+        return responseEntity;
     }
 }
